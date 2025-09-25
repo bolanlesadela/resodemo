ef0ec5f3dad744f9ff7cca7d1684f061a374a1aa
bolanlesadela <121446769+bolanlesadela@users.noreply.github.com>
2025-09-25 09:00:55 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+// // tailwind.config.js
+// export default {
+//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+//   theme: {
+//     extend: {
+//       colors: {
+//         "n-1": "var(--color-n-1)",
+//         "n-2": "var(--color-n-2)",
+//         "n-3": "var(--color-n-3)",
+//         "n-4": "var(--color-n-4)",
+//         "n-5": "var(--color-n-5)",
+//         "n-6": "var(--color-n-6)",
+//         "n-7": "var(--color-n-7)",
+//         "n-8": "var(--color-n-8)",
+//         "n-9": "var(--color-n-9)",
+//         "n-10": "var(--color-n-10)",
+//         "n-11": "var(--color-n-11)",
+//         "n-12": "var(--color-n-12)",
+//         "n-13": "var(--color-n-13)",
+//         "color-1": "var(--color-color-1)",
+//         "color-2": "var(--color-color-2)",
+//         "color-3": "var(--color-color-3)",
+//         "color-4": "var(--color-color-4)",
+//         "color-5": "var(--color-color-5)",
+//         "color-6": "var(--color-color-6)",
+//         "accent-color": "var(--accent-color)",
+//       },
+//       fontFamily: {
+//         sans: ["var(--font-sora)", "sans-serif"],
+//         code: ["var(--font-code)", "monospace"],
+//         grotesk: ["var(--font-grotesk)", "sans-serif"],
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
